07ca7007fc6014a44cd24c12427495c84b935337
jose-farley <jose.farley.sousa@gmail.com>
2023-10-15 17:45:34 -0300

    Adicionando conexão com banco de dados local.

---
+++ src/main/java/br/com/farley/todolist/user/IUserRepository.java
@@ -0,0 +1,12 @@
+package br.com.farley.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    
+    UserModel findByUserName(String userName);
+}
--- src/main/java/br/com/farley/todolist/user/UserController.java
+++ src/main/java/br/com/farley/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.farley.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private IUserRepository repository;
 
     @PostMapping("")
-    public void create(@RequestBody UserModel user){
-        System.out.println("-------------------- Dados do usuário ------------------");
-        System.out.println(user.name);
-        System.out.println(user.userName);
-        System.out.println(user.password);
+    public ResponseEntity create(@RequestBody UserModel user){
+        UserModel exist =  this.repository.findByUserName(user.getUserName());
+        if(exist != null ) {
+            System.out.println("Usuário ja existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        UserModel received =  this.repository.save(user);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(received);
     }
 }
